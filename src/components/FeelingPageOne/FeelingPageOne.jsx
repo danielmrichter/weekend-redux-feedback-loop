2eef4fa62d47a1ad8289b8f6d0958af0fdb132b2
Daniel Richter <drichter3697@gmail.com>
2024-08-18 12:45:56 -0500

    some styling done

--- src/components/FeelingPageOne/FeelingPageOne.jsx
+++ src/components/FeelingPageOne/FeelingPageOne.jsx
@@ -1,10 +1,11 @@
+import { Box, Button, InputLabel, Paper, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
 export default function FeelingPageOne() {
-  const feeling = useSelector(store => store.feeling)
-  useEffect(() => setFeelingInput(feeling), [])
+  const feeling = useSelector((store) => store.feeling);
+  useEffect(() => setFeelingInput(feeling), []);
   const [feelingInput, setFeelingInput] = useState(``);
   const history = useHistory();
   const dispatch = useDispatch();
@@ -18,18 +19,45 @@ export default function FeelingPageOne() {
   };
 
   return (
-    <form onSubmit={handlePageOneSubmit}>
-      <input
-        id="feeling"
-        required
-        data-testid="input"
-        type="number"
-        placeholder="Number 1-6"
-        value={feelingInput}
-        onChange={(e) => setFeelingInput(e.target.value)}
-      />
-      <label htmlFor="feeling">How are you feeling today?</label>
-      <button data-testid="next">NEXT PAGE</button>
-    </form>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      alignContent="center"
+      sx={{ mt: "10%" }}
+    >
+      <Paper
+        elevation={1}
+        sx={{
+          width: "40%",
+          display: "flex",
+          flexDirection: "column",
+          rowGap: 5,
+        }}
+      >
+        <form onSubmit={handlePageOneSubmit}>
+          <InputLabel sx={{ pb: 2 }} htmlFor="feeling">
+            How are you feeling today?
+          </InputLabel>
+          <TextField
+            autoFocus
+            inputProps={{ "data-testid": "input" }}
+            id="feeling"
+            required={true}
+            type="number"
+            placeholder="Number 1-6"
+            value={feelingInput}
+            onChange={(e) => setFeelingInput(e.target.value)}
+          />
+        </form>
+        <Button
+          onClick={handlePageOneSubmit}
+          variant="outlined"
+          data-testid="next"
+        >
+          NEXT PAGE
+        </Button>
+      </Paper>
+    </Box>
   );
 }
--- src/components/SupportedPageThree/SupportedPageThree.jsx
+++ src/components/SupportedPageThree/SupportedPageThree.jsx
@@ -1,11 +1,12 @@
+import { Box, Button, InputLabel, Paper, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function SupportedPageThree() {
   const supported = useSelector((store) => store.support);
-  useEffect(() => setsupportedInput(supported));
-  const [supportedInput, setsupportedInput] = useState(``);
+  useEffect(() => setSupportedInput(supported), []);
+  const [supportedInput, setSupportedInput] = useState(``);
   const history = useHistory();
   const dispatch = useDispatch();
   const handlePageThreeSubmit = (e) => {
@@ -17,21 +18,46 @@ export default function SupportedPageThree() {
     history.push(`/pageFour`);
   };
   return (
-    <>
-      <button onClick={() => history.push(`/pageTwo`)}>Go Back</button>
-      <form onSubmit={handlePageThreeSubmit}>
-        <label htmlFor="support">How well are you being supported?</label>
-        <input
-          id="support"
-          required
-          data-testid="input"
-          type="number"
-          placeholder="Number 1-6"
-          value={supportedInput}
-          onChange={(e) => setsupportedInput(e.target.value)}
-        />
-        <button data-testid="next">NEXT PAGE</button>
-      </form>
-    </>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      alignContent="center"
+      sx={{ mt: "10%" }}
+    >
+      <Paper
+        elevation={1}
+        sx={{
+          width: "40%",
+          display: "flex",
+          flexDirection: "column",
+          rowGap: 5,
+        }}
+      >
+        <Button onClick={() => history.push(`/pageTwo`)}>Go Back</Button>
+        <form onSubmit={handlePageThreeSubmit}>
+          <InputLabel sx={{ pb: 2 }} htmlFor="support">
+            How well are you being supported?
+          </InputLabel>
+          <TextField
+            autoFocus
+            inputProps={{ "data-testid": "input" }}
+            id="support"
+            required
+            type="number"
+            placeholder="Number 1-6"
+            value={supportedInput}
+            onChange={(e) => setSupportedInput(e.target.value)}
+          />
+        </form>
+        <Button
+          onClick={handlePageThreeSubmit}
+          variant="outlined"
+          data-testid="next"
+        >
+          NEXT PAGE
+        </Button>
+      </Paper>
+    </Box>
   );
 }
